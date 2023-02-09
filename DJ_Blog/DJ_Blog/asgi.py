82f4e59228d8a9ad7777306abe1324d9b3b6fd38
adivaste <adivaste@outlook.com>
2023-02-09 20:32:27 +0530

    🔰Started working on it

---
+++ DJ_Blog/DJ_Blog/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for DJ_Blog project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "DJ_Blog.settings")
+
+application = get_asgi_application()
