168c1f4d35d5c512150bead1af0cf6503ef62c98
AV <92280537+adivaste@users.noreply.github.com>
2023-03-02 18:15:44 +0000

    Made changes in UI & working on Request Handling

--- theme/static_src/tailwind.config.js
+++ theme/static_src/tailwind.config.js
@@ -42,7 +42,11 @@ module.exports = {
         // '../../**/*.py'
     ],
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                sans: ['"PT Sans"', 'sans-serif']
+              }          
+        },
     },
     plugins: [
         /**
