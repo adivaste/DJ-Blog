7744c4ec19c910b7164c777be277ba80bfd9a967
AV <92280537+adivaste@users.noreply.github.com>
2023-03-14 17:19:37 +0000

    Made  some changes

--- main/utils.py
+++ main/utils.py
@@ -3,7 +3,5 @@ def calculate_time_to_read(text):
     total_words = text.split()
 
     total_time_in_min = (int)(len(total_words)/speed_word_per_minute)
-    if total_time_in_min == 0:
-        return "Less than 1"
     return str(total_time_in_min)
 
