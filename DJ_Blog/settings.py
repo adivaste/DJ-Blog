b6b7f32e8ab587678525d3f77dbbbb60e248cefe
AV <92280537+adivaste@users.noreply.github.com>
2023-03-14 11:59:53 +0000

    Added Editor

--- DJ_Blog/settings.py
+++ DJ_Blog/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     "theme",
     "django_browser_reload",
     "widget_tweaks",
-    "ckeditor",
+    "tinymce",
 ]
 
 TAILWIND_APP_NAME = "theme"
--- main/models.py
+++ main/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
-from ckeditor.fields import RichTextField
+from tinymce.models import HTMLField
 
 # Create your models here.
 class Post(models.Model):
       title = models.CharField(max_length=255)
-      content = RichTextField(blank=True,null=True)
+      content = HTMLField(blank=True,null=True)
       publish_date = models.DateTimeField(auto_now_add=True)
       author = models.ForeignKey('Author', on_delete=models.CASCADE)
       views = models.IntegerField(default=0)
